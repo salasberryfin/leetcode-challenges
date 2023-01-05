00f8495b873b24606233410e31cbd289bdb310ab
salasberryfin <salash66@gmail.com>
2023-01-05 23:32:23 +0100

    94 Binary tree inorder traversal: add doc

--- go/94-binary-tree-inorder-traversal/solution.go
+++ go/94-binary-tree-inorder-traversal/solution.go
@@ -1,3 +1,13 @@
+/*
+Inorder traversal: getting all elements from left side to right side
+Recursive method:
+	1. Check if root is nil: return
+	2. Call recursively with tree.Left -> append result to slice of tree.Val
+		a. Instead of printing, we're storing it in an array
+		b. If printing -> print right after the recursive call with tree.Left
+	3. Append current value to result slice -> this is what creates the left to right order
+	4. Call recursively with tree.Right
+*/
 package main
 
 import "fmt"
