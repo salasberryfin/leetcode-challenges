429951743216405990261f7f4fa9c5fc1b5469ec
Carlos Salas <carlos.salas@suse.com>
2024-01-05 19:04:56 +0100

    update daily problem in go as well

--- go/0300-longest-increasing-subsequence/solution.go
+++ go/0300-longest-increasing-subsequence/solution.go
@@ -2,13 +2,14 @@
 Hard problem to understand at first sight
 
 The key is to iterate backwards and keep track of each of the longest increasing subsequence (LIS) for each element
-	- e.g. the last element will always have a LIS=1
-	- the second last will be 1+LIS[last] if last element < second last element
-	- and keep going...
+- e.g. the last element will always have a LIS=1
+- the second last will be 1+LIS[last] if last element > second last element
+- and keep going...
+
 Start by creating a LIS array to store each element's LIS, and initiliaze to 1s
-When checking a element that has multiple other elements to the right side of the array:
-	- there are multiple options to choose from
-	- check all of them and keep track of the max LIS of all of them
+When checking an element that has multiple other elements to the right side of the array:
+- there are multiple options to choose from
+- check all of them and keep track of the max LIS of all of them
 */
 package main
 
